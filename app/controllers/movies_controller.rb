4b017e6ad32a5ecf2e09ebff5b64e88bc439c8fe
James McJunkin <jmcj88@gmail.com>
2019-08-18 01:43:20 +0000

    Form Create

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,22 @@
 class MoviesController < ApplicationController
 
   def index
+    @movies = Movie.all
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    Movie.create(movie_params)
+    redirect_to root_path
+  end
+
+private
+
+  def movie_params
+    params.require(:movie).permit(:name, :description)
+  end
+
 end
